a1bd0cb4ba16419e16fd061b1940fb6c8bfc91b2
nahid-linux-desktop <b.m.shamsnahid@gmail.com>
2021-03-02 22:31:17 +0600

    Fix empty directory features

--- utils/structure-formatter.js
+++ utils/structure-formatter.js
@@ -2,8 +2,11 @@ const fs = require('fs');
 
 const {FILE_TYPE, FOLDER_TYPE} = require('../constants')
 
-const structureFormatter = (structure = [], parentPath = '', basePath) => 
-  structure
+const structureFormatter = (structure = [], parentPath = '', basePath) => {
+  if (!Array.isArray(structure)) {
+    return;
+  }
+  return structure
   .filter(node => {
     const ignorePattern = /^.ignore/i;
     const shouldIgnore = ignorePattern.test(node?.name);
@@ -38,6 +41,7 @@ const structureFormatter = (structure = [], parentPath = '', basePath) =>
           : ''
     };
   });
+}
 
 module.exports = {
   structureFormatter
